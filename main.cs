6551e5baf76abc17baea4cd9d052326ee8c383ba
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-04-05 17:17:29 +0000

    cs file

--- main.cs
+++ main.cs
@@ -1,7 +1,35 @@
+// Created by: Marshall Demars
+// Created on: Mar 2022
+//
+// This program accepts user input
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        double radiusSphere;
+        double volumeSphere;
+
+        // input
+        Console.WriteLine("This program calculates the volume of a sphere.");
+        Console.WriteLine("");
+        Console.WriteLine("the formula for the volume is = 4/3 x π x r³");
+        Console.WriteLine("");
+        Console.WriteLine("please enter the measurements (km)");
+        Console.WriteLine("");
+
+        Console.Write("Input the measurement for the radius: ");
+        radiusSphere = Convert.ToDouble(Console.ReadLine());
+
+        // process
+        volumeSphere = 4.0 / 3.0 * Math.PI * Math.Pow(radiusSphere, 3);
+
+        // output
+        Console.WriteLine("");
+        Console.WriteLine("The volume of the sphere is: " + volumeSphere.ToString("0.00") + " km³");
+        Console.WriteLine("\nDone.");
+    }
 }
